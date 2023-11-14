a0ab4cb81d9e7372b13b3e13e01a3863291e2836
Junho Choi <tig9659@ajou.ac.kr>
2023-11-15 02:08:23 +0900

    feat: InputView
    docs: 기능 요구사항 정리
    사용자 입력 및 입력 예외(에러)처리

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,18 @@
 package christmas;
 
+import domain.Menu;
+import view.InputView;
+
+import java.util.List;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
+        InputView inputView = new InputView();
+        int date = inputView.readDate();
+
+        List<Menu> menus = inputView.readMenus();
     }
 }
